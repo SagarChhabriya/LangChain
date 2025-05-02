69bc79bb4208e0ed7bbc8dbf3eb87ef46b518629
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2025-05-02 19:29:03 +0500

    add google gemini llm code demo

---
+++ 03-models/3.2-chat-models/3.2.2-anthropic-claude-chatmodel.py
@@ -0,0 +1,11 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat_model = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
+result = chat_model.invoke("What is the current rate of US dollar?")
+
+print(result)
+
