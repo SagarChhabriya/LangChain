ede0c3bcc70c4cfcc93c4b8c901a5ebac8fcc96b
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2025-05-03 22:18:28 +0500

    Add type conversion pydantic

---
+++ 05-structured-outputs/5.9-pydantic-type-coercing.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Student(BaseModel):
+    name: str = "Sagar"
+    age: Optional[int] = None 
+
+new_student = {"age":"18"} # implicit type conversion
+student = Student(**new_student)
+print(student)
+
+# output:
+# name='Sagar' age=18
