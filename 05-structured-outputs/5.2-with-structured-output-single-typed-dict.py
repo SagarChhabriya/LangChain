766c233b6bc72568883b3d6ef1ebf1fbb05ee343
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2025-05-03 22:16:18 +0500

    Add with struc output single typed dict

---
+++ 05-structured-outputs/5.2-with-structured-output-single-typed-dict.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from typing import TypedDict
+
+load_dotenv()
+
+chat_model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+
+class Review(TypedDict):
+    summary:str
+    sentiment:str
+
+struc_chat_model = chat_model.with_structured_output(Review)
+
+result = struc_chat_model.invoke("""The hardware is great, but the software feels bloated. 
+There are too many pre-installed apps that I can't remove. Also, the UI looks outdated 
+compared to other brands. Hoping for a software update to fix this.
+""")
+
+print(result)
